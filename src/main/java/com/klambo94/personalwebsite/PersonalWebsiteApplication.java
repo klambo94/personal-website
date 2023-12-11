6791aa82094471fd2abf7eb9dfac14c05aed5b44
Kendra Lamb <klambo94@gmail.com>
2023-12-11 13:23:08 -0500

    removing nested folder

--- src/main/java/com/klambo94/personalwebsite/PersonalWebsiteApplication.java
+++ src/main/java/com/klambo94/personalwebsite/PersonalWebsiteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalWebsiteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PersonalWebsiteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteApplication.class, args);
+	}
 
 }
