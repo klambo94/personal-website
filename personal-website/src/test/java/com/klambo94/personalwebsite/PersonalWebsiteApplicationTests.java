5fda684525f2b6a23b5b8b741b83012e12111cd3
Kendra Lamb <klambo94@gmail.com>
2023-12-11 11:54:53 -0500

    Initial Project commit

---
+++ personal-website/src/test/java/com/klambo94/personalwebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klambo94.personalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
